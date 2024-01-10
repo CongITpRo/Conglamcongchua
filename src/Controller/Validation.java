3977c8e028dfbd111e62c4c6192d6dcab08d1658
Admin <Admin@Admin>
2024-01-10 13:58:05 +0700

    Bai count

---
+++ src/Controller/Validation.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import java.util.Scanner;
+
+
+public class Validation {
+
+    public String getString(String msg) {
+        System.out.print(msg + " : ");
+        return new Scanner(System.in).nextLine();
+    }
+}
+
